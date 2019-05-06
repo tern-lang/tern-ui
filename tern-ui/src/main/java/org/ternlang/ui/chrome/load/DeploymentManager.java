b7dce80d2d25cfe406ed2417d856c6d5d1961fb4
ngallagher <gallagher_niall@yahoo.com>
2019-05-06 15:38:40 +0100

    Update deployment path

--- tern-ui/src/main/java/org/ternlang/ui/chrome/load/DeploymentManager.java
+++ tern-ui/src/main/java/org/ternlang/ui/chrome/load/DeploymentManager.java
@@ -2,8 +2,8 @@ package org.ternlang.ui.chrome.load;
 
 public class DeploymentManager {
 
-   public static void deploy(String libraryPath, Class mainClass, String[] arguments) {
-     String[] libraryPaths = LibraryLoader.loadFrom(libraryPath);
+   public static void deploy(String deployPath, Class mainClass, String[] arguments) {
+     String[] libraryPaths = LibraryLoader.loadFrom(deployPath);
      ApplicationLauncher.launch(mainClass, arguments, libraryPaths);
    }
 }
