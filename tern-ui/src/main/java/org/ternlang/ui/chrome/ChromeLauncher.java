86a921576a49c648b4b4d0af3d69886d1d8aa0d4
ngallagher <gallagher_niall@yahoo.com>
2019-05-06 16:04:42 +0100

    Make the main class a string

--- tern-ui/src/main/java/org/ternlang/ui/chrome/ChromeLauncher.java
+++ tern-ui/src/main/java/org/ternlang/ui/chrome/ChromeLauncher.java
@@ -8,7 +8,8 @@ import static org.ternlang.ui.chrome.ChromeBrowser.LIBRARY_PATH;
 public class ChromeLauncher {
 
     public static void main(String[] args) throws Exception {
-        DeploymentTask task = DeploymentManager.deploy(LIBRARY_PATH, ChromeBrowser.class, args);
+        String mainClass = ChromeBrowser.class.getName();
+        DeploymentTask task = DeploymentManager.deploy(LIBRARY_PATH, mainClass, args);
         Runnable forkTask = task.getForlTask();
 
         forkTask.run();
